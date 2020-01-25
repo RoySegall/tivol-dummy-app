25964985cbccbc25fd28125ce39968e85bac1b72
Roy Segall <roy@segall.io>
2020-01-25 18:02:02 +0200

    Adding company model

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from dummyapp.models import Animal
+from dummyapp.models import Animal, Company
 
 admin.site.register(Animal)
+admin.site.register(Company)
--- models.py
+++ models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
-# Create your models here.
-from django.db.models import CharField, IntegerField
+from django.db.models import CharField, IntegerField, DateTimeField
 
 
 class Animal(models.Model):
     animal_name = CharField(max_length=25)
     sound = CharField(max_length=25)
     number_of_legs = IntegerField()
+
+
+class Company(models.Model):
+    name = CharField(max_length=25)
+    description = CharField(max_length=25)
+    founded_at = DateTimeField()
+    founded_by = CharField(max_length=25)
