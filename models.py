7ef3c8a88adc38f4d23f217ffc7785d4e471c7b1
Roy Segall <roy@segall.io>
2020-01-26 21:08:45 +0200

    Adding IDs to the migration files

--- models.py
+++ models.py
@@ -7,9 +7,15 @@ class Animal(models.Model):
     sound = CharField(max_length=25)
     number_of_legs = IntegerField()
 
+    def __str__(self):
+        return self.animal_name
+
 
 class Company(models.Model):
     name = CharField(max_length=25)
     description = CharField(max_length=25)
     founded_at = DateTimeField()
     founded_by = CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
